b309b8fb2220065b2ddf3e7cbc8b0ff6e77c8050
petersembi <petersembi@gmail.com>
2022-09-17 10:49:16 +0300

    modified:   employee_register/forms.py 	modified:   employee_register/models.py 	modified:   employee_register/urls.py 	modified:   employee_register/views.py 	modified:   templates/employee_register/employee_form.html 	modified:   templates/employee_register/employee_list.html

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import EmployeeForm
+from .models import Employee
+
+# from http.client import HTTPResponse
+# from django.shortcuts import get_object_or_404, render
+# from django.template import loader
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.urls import reverse
 # Create your views here.
 
 def employee_list(request):
-    return render(request, "employee_register/employee_list.html")
+    context  = {'employee_list' : Employee.objects.all()}
+    return render(request, "employee_register/employee_list.html",context)
 
-def employee_form(request):
-    form = EmployeeForm()
-    return render(request, "employee_register/employee_form.html", {'form':form})
+def employee_form(request, id=0):
+    if request.method == "GET":
+        if id == 0:
+            form = EmployeeForm()
+        else:
+            employee = Employee.objects.get(pk=id)
+            form = EmployeeForm(instance=employee)
+            
+        return render(request, "employee_register/employee_form.html", {'form':form})
+    else:
+        if id == 0:         
+            form = EmployeeForm(request.POST)
+        else:
+            employee = Employee.objects.get(pk=id)
+            form = EmployeeForm(request.POST, instance = employee)
+        # check whether the form is valid
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
 
-def employee_delete(request):
-    return
+def employee_delete(request,id):
+    employee = Employee.objects.get(pk=id)
+    employee.delete()
+    return redirect('/employee/list')
+
 
